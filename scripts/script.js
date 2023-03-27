7635c36d16349559697ad94e5348b90b02e09fff
Matheus-Natan <m.gomesmg1904@gmail.com>
2023-03-27 08:31:49 -0300

    Atualização No Estilo e Algumas Alterações no JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,36 @@
-function updateCycle() {
-    let select = document.querySelector("#select_cycle");
-    let optionValue = select.options[select.selectedIndex];
-    let value = optionValue.value;
-
-    if (value == "thinning") {
-        let thinningLayer = document.getElementById('thinning');
-        thinningLayer.style.display = 'block';
+function getElement(id) {
+    let element = parseFloat(document.querySelector(`#${id}`).value);
+    if (isNaN(element) || element === "") {
+        return false;
     } else {
-        let thinningLayer = document.getElementById('thinning');
-        thinningLayer.style.display = 'none';
-    }
-
-    if (value == "facing") {
-        let facingLayer = document.getElementById('facing');
-        facingLayer.style.display = 'block';
-    } else {
-        let facingLayer = document.getElementById('facing');
-        facingLayer.style.display = 'none';
+        return element; // retorna o valor do input
     }
 }
+
+function validateInputsRoughing() {
+    const inputs = ["initial_diameter", "starting_length", "X_axis_finishing_allowance", "Z_axis_finishing_allowance", "increment_per_pass", "advance_in_roughing"];
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (!getElement(inputs[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function validateInputsFacing() {
+    const inputs = ["final_diameter_of_facing", "end_position_of_facing", "increment_per_facing_pass", "advance_in_facing"];
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (!getElement(inputs[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 
 function filterDecimalPlaces(number) {
 
@@ -38,13 +50,13 @@ function filterDecimalPlaces(number) {
 
 function generateRoughingCode() {
 
-    let initial_diameter = parseFloat(document.getElementById('initial_diameter').value) + 4;
-    let starting_length = parseFloat(document.getElementById("starting_length").value) + 2;
-    let xAxisFinishingAllowance = parseFloat(document.getElementById("X_axis_finishing_allowance").value);
-    let zAxisFinishingAllowance = parseFloat(document.getElementById("Z_axis_finishing_allowance").value);
-    let increment_per_pass = parseFloat(document.getElementById("increment_per_pass").value);
-    let subProgram = document.getElementById("subProgram").value;
-    let advance_in_roughing = parseFloat(document.getElementById("advance_in_roughing").value);
+    let initial_diameter = getElement("initial_diameter") + 4;
+    let starting_length = getElement("starting_length") + 2;
+    let xAxisFinishingAllowance = getElement("X_axis_finishing_allowance");
+    let zAxisFinishingAllowance = getElement("Z_axis_finishing_allowance");
+    let increment_per_pass = getElement("increment_per_pass");
+    let subProgram = document.querySelector("#subProgram").value;
+    let advance_in_roughing = getElement("advance_in_roughing");
 
     let preFinishing = document.getElementById("pre_finishing");
     let roughingResult = document.getElementById("roughing_result");
@@ -54,17 +66,17 @@ function generateRoughingCode() {
     if (preFinishing.checked) {
         roughingResult.innerHTML = `G66 X${filterDecimalPlaces(initial_diameter)} Z${filterDecimalPlaces(starting_length)} I${filterDecimalPlaces(xAxisFinishingAllowance)} K${filterDecimalPlaces(zAxisFinishingAllowance)} U1 W${filterDecimalPlaces(increment_per_pass)} ${subProgram} F${filterDecimalPlaces(advance_in_roughing)}#`
     } else {
-        roughingResult .innerHTML = `G66 X${filterDecimalPlaces(initial_diameter)} Z${filterDecimalPlaces(starting_length)} I${filterDecimalPlaces(xAxisFinishingAllowance)} K${filterDecimalPlaces(zAxisFinishingAllowance)} W${filterDecimalPlaces(increment_per_pass)} ${subProgram} F${filterDecimalPlaces(advance_in_roughing)}#`
+        roughingResult.innerHTML = `G66 X${filterDecimalPlaces(initial_diameter)} Z${filterDecimalPlaces(starting_length)} I${filterDecimalPlaces(xAxisFinishingAllowance)} K${filterDecimalPlaces(zAxisFinishingAllowance)} W${filterDecimalPlaces(increment_per_pass)} ${subProgram} F${filterDecimalPlaces(advance_in_roughing)}#`
     }
 
 }
 
 function generateFacingCode() {
 
-    let finalDiameterOfFacing = parseFloat(document.getElementById('final_diameter_of_facing').value);
-    let endPositionOfFacing = parseFloat(document.getElementById('end_position_of_facing').value);
-    let incrementPerFacingPass = parseFloat(document.getElementById('increment_per_facing_pass').value);
-    let advanceInFacing = parseFloat(document.getElementById('advance_in_facing').value);
+    let finalDiameterOfFacing = getElement('final_diameter_of_facing');
+    let endPositionOfFacing = getElement('end_position_of_facing');
+    let incrementPerFacingPass = getElement('increment_per_facing_pass');
+    let advanceInFacing = getElement('advance_in_facing');
 
     let toolAngularIndent = document.getElementById("tool_angular_indent");
     let resultFacing = document.getElementById("result_facing");
@@ -77,4 +89,20 @@ function generateFacingCode() {
         resultFacing.innerHTML = `G75 X${filterDecimalPlaces(finalDiameterOfFacing)} Z${filterDecimalPlaces(endPositionOfFacing)} K${filterDecimalPlaces(incrementPerFacingPass)} F${filterDecimalPlaces(advanceInFacing)}#`
     }
 
+}
+
+function onGenerateRoughingCodeClicked() {
+    if (validateInputsRoughing()) {
+        generateRoughingCode();
+    } else {
+        alert("Por favor, preencha todos os campos corretamente.");
+    }
+}
+
+function onGenerateFacingCodeClicked() {
+    if (validateInputsFacing()) {
+        generateFacingCode();
+    } else {
+        alert("Por favor, preencha todos os campos corretamente.");
+    }
 }
